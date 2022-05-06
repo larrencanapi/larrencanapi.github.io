aea1cea880adc3306821b6d0ce505979b45af436
Larren Canapi <42918944+SeekNDstr0y@users.noreply.github.com>
2022-05-05 19:19:05 -0700

    Refactored website code

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 const menuButton = document.getElementsByClassName('menu-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-sections')[0];
 
+// activates mobile menu list open
 menuButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
 
+// close mobile menu when link is clicked
 var links = navbarLinks.getElementsByTagName('a');
-
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', () => {
         navbarLinks.classList.remove('active');
